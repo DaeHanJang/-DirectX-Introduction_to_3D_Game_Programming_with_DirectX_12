558d3526b7e56755aeb7df9c952e52945aa75271
DeaHanJang <lcjdh0906@gmail.com>
2025-03-17 12:01:08 +0900

    20250317

---
+++ Common/GameTimer.h
@@ -0,0 +1,33 @@
+//***************************************************************************************
+// GameTimer.h by Frank Luna (C) 2011 All Rights Reserved.
+//***************************************************************************************
+
+#ifndef GAMETIMER_H
+#define GAMETIMER_H
+
+class GameTimer {
+public:
+	GameTimer();
+
+	float TotalTime() const; //초 단위
+	float DeltaTime() const; //초 단위
+	
+	void Reset(); //메시지 루프 이전에 호출해야 함
+	void Start(); //타이머를 시작 또는 재개할 때 호출해야 함
+	void Stop();  //타이머를 정지(일시 정지)할 때 호출해야 함
+	void Tick();  //매 프레임 호출해야 함
+
+private:
+	double mSecondsPerCount;
+	double mDeltaTime;
+
+	__int64 mBaseTime;
+	__int64 mPausedTime;
+	__int64 mStopTime;
+	__int64 mPrevTime;
+	__int64 mCurrTime;
+
+	bool mStopped;
+};
+
+#endif //GAMETIMER_H
